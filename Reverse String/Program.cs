211385b29700fba5991bd4a7d8c97a89115b430a
Samaria Anish <samariaanish2000@gmail.com>
2023-02-14 16:16:16 +0530

    Add project files.

---
+++ Reverse String/Program.cs
@@ -0,0 +1,13 @@
+﻿string requiredStr, reversedString;
+requiredStr = "SAMARIA";
+reversedString = "";
+Console.WriteLine("Value of given String is: {0}", requiredStr);
+int a;
+a = requiredStr.Length - 1;
+while (a >= 0)
+{
+    reversedString = reversedString + requiredStr[a];
+    a--;
+}
+Console.WriteLine("Reversed string is: {0}", reversedString);
+Console.ReadLine();
